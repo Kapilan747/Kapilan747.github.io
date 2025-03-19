fd076dd3d95093f9c04c96a5e638410e5064d496
Kapilan S D <Kapilan357@gmail.com>
2025-03-19 22:35:41 +0530

    Fix Vite config and script path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "Kapilan747.github.io",
+  base: "/Kapilan747.github.io/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
-
