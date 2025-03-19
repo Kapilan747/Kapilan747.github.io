666d647bbaf565fea38a334ea723a1e18427ab25
Kapilan S D <Kapilan357@gmail.com>
2025-03-19 23:11:31 +0530

    Fixed Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   base: "/Kapilan747.github.io/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
 });
